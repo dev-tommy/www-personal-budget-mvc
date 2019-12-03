d151fd595a69625214adf47d767dd001cc8fca9b
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-03 15:46:10 +0100

    Add query check

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'WWW public test';
+/**
+ * Front controller
+ *
+ * PHP version 7.3
+ *
+ * Author: Tomasz Frydrychowicz
+ * e-mail: tomasz.frydrychowicz.programista@gmail.com
+*/
+
+echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
