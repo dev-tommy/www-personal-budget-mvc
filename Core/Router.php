73aae1afdcf5db9dba456eb7a838fb957ef5c464
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-03 16:35:12 +0100

    Add Router class

---
+++ Core/Router.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Router v0.1
+ *
+ * PHP version 7.3
+ *
+ * Author: Tomasz Frydrychowicz
+ * e-mail: tomasz.frydrychowicz.programista@gmail.com
+ */
+
+ class Router
+ {
+
+ }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Front controller
+ * Front controller v0.1
  *
  * PHP version 7.3
  *
@@ -9,4 +9,14 @@
  * e-mail: tomasz.frydrychowicz.programista@gmail.com
 */
 
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
