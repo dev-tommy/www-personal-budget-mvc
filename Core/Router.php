02be8b2fdbfad0308d1e5b3b630e8a8dd997c1cd
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-03 16:59:29 +0100

    Add basic functions

--- Core/Router.php
+++ Core/Router.php
@@ -11,5 +11,30 @@
 
  class Router
  {
+     protected $routes = [];
+     protected $params = [];
 
+     public function getRoutes()
+     {
+         return $this->routes;
+     }
+
+     public function match($url)
+     {
+         foreach ($this->routes as $route => $params) {
+             if ($url == $route) {
+                 $this->params = $params;
+                 return true;
+             }
+         }
+         return false;
+     }
+
+     public function getParams()
+     {
+         return $this->params;
+     }
  }
+
+ ?>
+
