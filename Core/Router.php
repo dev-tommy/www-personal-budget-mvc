49ba6d7c6ca46bb27349567858fadaf07c82df92
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-03 17:15:40 +0100

    Add method add()

--- Core/Router.php
+++ Core/Router.php
@@ -11,30 +11,35 @@
 
  class Router
  {
-     protected $routes = [];
-     protected $params = [];
+    protected $routes = [];
+    protected $params = [];
 
-     public function getRoutes()
-     {
-         return $this->routes;
-     }
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
 
-     public function match($url)
-     {
-         foreach ($this->routes as $route => $params) {
-             if ($url == $route) {
-                 $this->params = $params;
-                 return true;
-             }
-         }
-         return false;
-     }
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 
-     public function getParams()
-     {
-         return $this->params;
-     }
- }
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
 
- ?>
+    public function getParams()
+    {
+        return $this->params;
+    }
+}
+
+?>
 
