19127571b26d9be839f30fa55b5d47c29f2cd784
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-03 17:16:59 +0100

    Add check correct query

--- public/index.php
+++ public/index.php
@@ -9,8 +9,6 @@
  * e-mail: tomasz.frydrychowicz.programista@gmail.com
 */
 
-//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
-
 /**
  * Routing
  */
@@ -19,4 +17,16 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
