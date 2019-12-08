9067f14579774c6453174f9b20bf93deb0992d29
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-08 16:26:54 +0100

    Fixed $file

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 // autoloader controllers
 spl_autoload_register(function ($class) {
     $root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . 'php';
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
     if (is_readable($file)) {
         require $file;
     }
