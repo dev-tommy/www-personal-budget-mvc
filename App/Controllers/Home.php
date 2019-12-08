6172287ba20f1bb98600e6772536d11312d97d17
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-08 16:17:16 +0100

    Add main index page with namespace

---
+++ App/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+/**
+ * Incomes controller v0.1
+ *
+ * PHP version 7.3
+ *
+ * Created with course PHP MVC framework by author Dave Hollingworth
+ * e-mail: tomasz.frydrychowicz.programista@gmail.com
+ */
+
+class Home
+{
+    public function index()
+    {
+        echo "index home page";
+    }
+}
