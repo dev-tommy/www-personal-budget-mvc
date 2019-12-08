3b95ba1cfad6d3189832173f8d5c7f4bd0ed8c57
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-08 15:41:47 +0100

    Add incomes controller for test

---
+++ App/Controllers/Incomes.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Incomes controller v0.1
+ *
+ * PHP version 7.3
+ *
+ * Created with course PHP MVC framework by author Dave Hollingworth
+ * e-mail: tomasz.frydrychowicz.programista@gmail.com
+ */
+
+class Incomes
+{
+    public function index()
+    {
+        echo "index page";
+    }
+
+    public function add()
+    {
+        echo "Add new income";
+    }
+
+    public function edit()
+    {
+        echo "Edit income";
+    }
+
+    public function delete()
+    {
+        echo "Delete income";
+    }
+
+    public function rename()
+    {
+        echo "Rename income";
+    }
+
+    public function view()
+    {
+        echo "View income";
+    }
+
+}
+
+
+?>
