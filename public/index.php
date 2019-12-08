71ea6599f84f87df80e3c738c3fc2c0fa11033fc
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-08 19:10:08 +0100

    Add route to admin namespace

--- public/index.php
+++ public/index.php
@@ -27,6 +27,7 @@ $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('incomes', ['controller' => 'Incomes', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 
 /*
 $url = $_SERVER['QUERY_STRING'];
