6970967922df7a371ddb54a4ecaf4d26d650de92
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-08 17:13:30 +0100

    Fixed security hole - direct call action classes

--- Core/Router.php
+++ Core/Router.php
@@ -60,7 +60,7 @@ namespace Core;
                 $action = $this->params['action'];
                 $action = $this->convertToCamelCase($action);
 
-                if (is_callable([$controller_object, $action])) {
+                if (preg_match('/action$/i', $action) == 0) {
                     $controller_object->$action();
                 } else {
                     echo "Method $action (in controller $controller) not found";
