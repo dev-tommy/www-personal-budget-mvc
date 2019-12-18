96c87f43f532e493e36bb971aea0cc866a3db523
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-18 13:23:59 +0100

    Add show of args[]

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Home view</title>
 </head>
+
 <body>
-    <h1>Hello</h1>
+    <h1>Hello <?php echo htmlspecialchars($name); ?> !!</h1>
     <p>Home view test page</p>
+    <ul>
+        <?php foreach ($colours as $colour) : ?>
+            <li><?php echo htmlspecialchars($colour); ?></li>
+        <?php endforeach; ?>
+    </ul>
+
 </body>
+
 </html>
