cd347bbba15aea34c09084caf08ae9833739f83b
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-18 13:22:35 +0100

    Add $args[] params to render()

--- Core/View.php
+++ Core/View.php
@@ -13,8 +13,10 @@ namespace Core;
 
 class View
 {
-    public static function render($view)
+    public static function render($view, $args = [])
     {
+        extract($args, EXTR_SKIP);
+
         $file = "../App/Views/$view";
 
         if  (is_readable($file)) {
