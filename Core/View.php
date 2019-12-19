b4bd64deaaf2ac786b3089307a6340d2855ba85a
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-19 13:41:22 +0100

    Add renderTemplate()

--- Core/View.php
+++ Core/View.php
@@ -25,4 +25,16 @@ class View
             echo "$file not found";
         }
     }
+
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__).'/App/Views');
+            $twig = new \Twig\Environment($loader);
+        }
+
+        echo $twig->render($template, $args);
+    }
 }
