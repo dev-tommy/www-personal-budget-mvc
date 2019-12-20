1f3a3e4c8de242e2cc7b41cd21636740ef35bcb7
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-20 11:18:15 +0100

    Add getCorrectHttpResponseCode()

--- Core/Error.php
+++ Core/Error.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Core;
-
 /**
  * Base model
  *
@@ -13,6 +12,9 @@ namespace Core;
 
 class Error
 {
+    const PAGE_NOT_FOUND = 404;
+    const SERVER_ERROR = 500;
+
     private static function showDescription($exception)
     {
         echo "<h1>Fatal error</h1>";
@@ -20,6 +22,16 @@ class Error
         echo "<p>Message: '" . $exception->getMessage() . "'</p>";
         echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
         echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
+    }
+
+    private static function getCorrectHttpResponseCode($exception)
+    {
+        $code = $exception->getCode();
+        if ($code != Error::PAGE_NOT_FOUND) {
+            $code = Error::SERVER_ERROR;
+        }
+        http_response_code($code);
+        return $code;
     }
 
     public static function errorHandler($level, $message, $file, $line)
@@ -31,11 +43,7 @@ class Error
 
     public static function exceptionHandler($exception)
     {
-        $code = $exception->getCode();
-        if ($code != 404) {
-            $code = 500;
-        }
-        http_response_code($code);
+        $code = Error::getCorrectHttpResponseCode($exception);
 
         if (\App\Config::SHOW_ERRORS) {
             Error::showDescription($exception);
