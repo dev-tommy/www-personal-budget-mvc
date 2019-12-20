e0f42af5a40f84a4f10e3e5b71e61586b110fcd9
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-20 08:26:23 +0100

    Add const SHOW_ERRORS for developing mode

--- App/Config.php
+++ App/Config.php
@@ -13,8 +13,11 @@ namespace App;
 
 class Config
 {
+    //database config
     const DB_HOST = 'localhost';
     const DB_NAME = 'personalBudget';
     const DB_USER = 'root';
     const DB_PASSWORD = 'mysql';
+    //log config
+    const SHOW_ERRORS = true;
 }
