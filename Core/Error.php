805f481ddc43187ba4e2322ad5d728f6a6956ece
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-20 09:18:09 +0100

    Add show page 404 or  500 in production mode

--- Core/Error.php
+++ Core/Error.php
@@ -22,14 +22,18 @@ class Error
 
     public static function exceptionHandler($exception)
     {
+        $code = $exception->getCode();
+        if ($code != 404) {
+            $code = 500;
+        }
+        http_response_code($code);
+
         if (\App\Config::SHOW_ERRORS) {
-
-
-        echo "<h1>Fatal error</h1>";
-        echo "<p>Uncaught exception: '".get_class($exception)."'</p>";
-        echo "<p>Message: '".$exception->getMessage()."'</p>";
-        echo "<p>Stack trace:<pre>".$exception->getTraceAsString()."</pre></p>";
-        echo "<p>Thrown in '".$exception->getFile()."' on line ".$exception->getLine()."</p>";
+            echo "<h1>Fatal error</h1>";
+            echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
+            echo "<p>Message: '" . $exception->getMessage() . "'</p>";
+            echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
+            echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
         } else {
             $log = dirname(__DIR__) . '/logs/' . date('Y-m-d') . '.txt';
             ini_set('error_log', $log);
@@ -40,8 +44,11 @@ class Error
             $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
 
             error_log($message);
-            echo "<h1>An error occured</h1>";
+            if ($code == 404) {
+                echo "<h1>Page not found</h1>";
+            } else {
+                echo "<h1>An error occured</h1>";
+            }
         }
-
     }
 }
