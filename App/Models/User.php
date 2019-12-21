88a13cba2635faedb6b7ceb831175b76fcbdf1cf
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-21 15:58:34 +0100

    Add save()

--- App/Models/User.php
+++ App/Models/User.php
@@ -13,4 +13,18 @@ class User extends \Core\Model
         }
     }
 
+    public function save()
+    {
+        $sql = 'INSERT INTO users (name, email, password_hash
+                VALUES (:name, :email, :password_hash)';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $stmt->bindValue(':password_hash', $this->password, PDO::PARAM_STR);
+
+        $stmt->execute();
+    }
 }
