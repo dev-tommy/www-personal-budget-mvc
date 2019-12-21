0be0fa5c6472fad7c57702c40404553958631ce8
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-21 17:59:09 +0100

    Add show signup errors to signup page

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -30,7 +30,11 @@ class Signup extends \Core\Controller
         if ($user->save()) {
             View::renderTemplate('Signup/success.html');
         } else {
-            var_dump($user->errors);
+            View::renderTemplate('Signup/new.html',
+            [
+                'user' => $user
+            ]
+        );
         }
     }
 
