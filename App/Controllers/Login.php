e26a0e50f4f09597b6add93ca97d64a75211e4c3
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-23 14:14:55 +0100

    Add newAction()

---
+++ App/Controllers/Login.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Login controller v0.1
+ *
+ * PHP version 7.3
+ *
+ * Created with course PHP MVC framework by author Dave Hollingworth
+ * e-mail: tomasz.frydrychowicz.programista@gmail.com
+ */
+
+class Login extends \Core\Controller
+{
+    protected function before()
+    {
+    }
+
+    protected function after()
+    {
+    }
+
+    public function newAction()
+    {
+
+    }
+}
