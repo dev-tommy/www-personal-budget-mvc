ffe0a5e3aab130c91ce0b10cb811bed196ff65b4
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-12-30 08:54:15 +0100

    Add regenerate session id after log in

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -33,6 +33,7 @@ class Login extends \Core\Controller
     {
         $user = User::authenticate($_POST['email'], $_POST['password']);
         if ($user) {
+            session_regenerate_id(true);
             $_SESSION['user_id'] = $user->id;
             $this->redirect('/');
         } else {
