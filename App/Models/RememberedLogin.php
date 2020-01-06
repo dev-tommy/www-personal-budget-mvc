2cfe6db795d76797b14adda6423449c56b7f574a
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-06 13:19:52 +0100

    Add findByToken()

---
+++ App/Models/RememberedLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use \App\Token;
+use PDO;
+
+class RememberedLogin extends \Core\Model
+{
+    public static function findByToken($token)
+    {
+        $token = new Token($token);
+        $token_hash = $token->getHash();
+
+        $sql = 'SELECT * FROM remembered_logins
+                WHERE token_hash = :token_hash';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':token_hash', $token_hash, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+}
