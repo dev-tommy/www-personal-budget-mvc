29836abaa4c4b79fb8189fd00a179ca5c88e3229
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-06 10:36:05 +0100

    Add remember_me checkbox to login page

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -34,6 +34,7 @@ class Login extends \Core\Controller
     public function createAction()
     {
         $user = User::authenticate($_POST['email'], $_POST['password']);
+        $remember_me = isset($_POST['remember_me']);
         if ($user) {
             Auth::login($user);
             Flash::addMessage('Login successful');
@@ -41,7 +42,8 @@ class Login extends \Core\Controller
         } else {
             Flash::addMessage('Login unsuccessful');
             View::renderTemplate('Login/new.html', [
-                'email' => $_POST['email']
+                'email' => $_POST['email'],
+                'remember_me' => $remember_me
             ]);
         }
     }
