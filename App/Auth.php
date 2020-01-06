9f71522b8f7751c030d7353b2d80a1e436b4a32a
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-06 20:32:03 +0100

    Add forgerLogin()

--- App/Auth.php
+++ App/Auth.php
@@ -90,4 +90,18 @@ class Auth
             }
         }
     }
+
+    protected static function forgetLogin()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if ($cookie) {
+            $remembered_login = RememberedLogin::findByToken($cookie);
+
+            if ($remembered_login) {
+                $remembered_login->delete();
+            }
+            setcookie('remember_me', '', time() - 3600);
+        }
+    }
 }
