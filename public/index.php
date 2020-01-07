13f40d3bc80186dab7c95be21b3b52a2cb979e0b
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-07 14:26:09 +0100

    Update router links

--- public/index.php
+++ public/index.php
@@ -24,8 +24,10 @@ session_start();
 
 $router = new Core\Router();
 
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('incomes', ['controller' => 'Incomes', 'action' => 'index']);
+$router->add('', ['controller' => 'Signup', 'action' => 'new']);
+$router->add('incomes', ['controller' => 'Incomes', 'action' => 'add']);
+$router->add('expendes', ['controller' => 'Expendes', 'action' => 'add']);
+$router->add('balance', ['controller' => 'Balance', 'action' => 'show']);
 $router->add('signup', ['controller' => 'Signup', 'action' => 'new']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
 $router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
