044fbde9d80843e6fdc0df3048e621a2fe8b3d3d
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-14 12:01:16 +0100

    Add comment validation

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -82,6 +82,18 @@ class Income extends \Core\Model
                 }
             }
         }
+
+        if (!isset($this->comment)) {
+            $this->comment = '';
+        } else {
+            if (!preg_match('/^[a-zA-Z0-9 .,!]+$/', $this->comment)) {
+                $this->isValid['comment'] = 'is-invalid';
+                $this->warnings['comment'] = 'Dozwolone znaki to: a-z, A-Z, 0-9, spacja, kropka, przecinek';
+            } else if (strlen($this->comment) > 180 ) {
+                $this->isValid['comment'] = 'is-invalid';
+                $this->warnings['comment'] = 'Maksymalna długość komentarza to 180 znaków';
+            }
+        }
     }
 
     private function validateDate($date, $format = 'Y-m-d')
