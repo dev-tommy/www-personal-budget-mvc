cd958b39d11f4bcd2aad05d53e4063061438c997
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-19 13:07:49 +0100

    Add functions to calculate total amounts

--- App/Models/Balance.php
+++ App/Models/Balance.php
@@ -85,7 +85,52 @@ class Balance extends \Core\Model
         $db = static::getDB();
         $stmt = $db->query($sql);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         return $result;
+    }
+
+    public static function getTotalIncomesAmount()
+    {
+        $sql = "
+        SELECT
+            SUM(i.amount) AS 'Total'
+        FROM
+            incomes AS i,
+            incomes_category_default AS i_userid
+        WHERE
+            i_userid.id = i.income_category_assigned_to_user_id AND
+            i.date_of_income >= '2000-01-01' AND
+            i.date_of_income <= '2020-12-31' AND
+            i.user_id='4'
+        ";
+
+        $db = static::getDB();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result[0]['Total'];
+    }
+
+    public static function getTotalExpensesAmount()
+    {
+        $sql = "
+        SELECT
+            SUM(e.amount) AS 'Total'
+        FROM
+            expenses AS e,
+            expenses_category_default AS e_userid
+        WHERE
+            e_userid.id = e.expense_category_assigned_to_user_id AND
+            e.date_of_expense >= '2000-01-01' AND
+            e.date_of_expense <= '2020-12-31' AND
+            e.user_id='4'
+        ";
+
+        $db = static::getDB();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result[0]['Total'];
     }
 
     public static function getExpenses()
