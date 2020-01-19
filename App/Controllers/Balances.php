1b05859596ca2f7a7123923f2f6f07287d516125
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-19 19:52:56 +0100

    Add rand_color()

--- App/Controllers/Balances.php
+++ App/Controllers/Balances.php
@@ -27,6 +27,16 @@ class Balances extends Authenticated
         return $sum;
     }
 
+    function rand_color()
+    {
+        return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+    }
+
+    private function getChartElements($expenses)
+    {
+
+    }
+
     public function showForPeriodAction()
     {
         $alertShow = 'false';
