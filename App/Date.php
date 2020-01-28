7e565d6ec402a7b0d6363a2112752bf76da2f5df
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-01-28 13:44:22 +0100

    Remove constructor and getDate()

--- App/Date.php
+++ App/Date.php
@@ -4,18 +4,7 @@ namespace App;
 
 class Date
 {
-    protected $rawDate;
-
     public static $months = array('styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień');
-
-    public function __construct($date_value = null)
-    {
-        if ($date_value) {
-            $this->rawDate = $date_value;
-        } else {
-            $this->rawDate = strtotime("today");;
-        }
-    }
 
     public static function rawDate($convertedDate)
     {
@@ -25,11 +14,6 @@ class Date
     public static function convertDate($rawDate)
     {
         return date("Y-m-d", $rawDate);
-    }
-
-    public function getDate()
-    {
-        return Date::convertDate($this->rawDate);
     }
 
     public static function getTodayDate()
@@ -76,10 +60,4 @@ class Date
     {
         return Date::convertDate(strtotime(date("Y") . "-01-01"));
     }
-
-
-
-
-
-
 }
