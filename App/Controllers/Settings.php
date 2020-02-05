632e28bb7aefeaea0ddaf6abbc5b167f89b04f65
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-02-05 19:15:42 +0100

    Add data to render

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -2,13 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Models\Expense;
+use App\Models\Income;
+use App\Models\User;
 use \Core\View;
 
 class Settings extends Authenticated
 {
+    //get incomes, expenses, payment , user data
     public function showAction()
     {
-        View::renderTemplate('Settings/show.html');
+        $incomeCategories = Income::getAllCategory();
+        $expenseCategories = Expense::getAllCategory();
+        $paymentMethods = Expense::getAllPayments();
+        $user = User::findByID($_SESSION['user_id']);
+
+        View::renderTemplate('Settings/show.html', [
+            'incomeCategories' => $incomeCategories,
+            'expenseCategories' => $expenseCategories,
+            'paymentMethods' => $paymentMethods,
+            'user' => $user
+        ]);
     }
 }
 
