e241cd7f33238bdb9c59300502b35d11e55ca1d6
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-02-19 11:10:55 +0100

    Add deleteCategory()

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -59,6 +59,31 @@ class Expense extends \Core\Model
         }
     }
 
+    public function deleteCategory()
+    {
+        if ($this->validateId() == 'true') {
+
+            if (empty($this->isEmptyCategory())) {
+
+                $userId = $_SESSION['user_id'];
+
+                $sql = "DELETE FROM expenses_category_assigned_to_userid_$userId WHERE id = :categoryId";
+
+                $db = static::getDB();
+                $stmt = $db->prepare($sql);
+                $stmt->bindValue(':categoryId', $this->id, PDO::PARAM_INT);
+                $stmt->execute();
+
+                $numberOfDeletedRows = $stmt->rowCount();
+                return "Kategoria została usunięta. Usunięto " . $numberOfDeletedRows;
+            } else {
+                return "Kategoria zawiera przychody. Czy chcesz ją usunąć? ";
+            }
+        } else {
+            return "Nie znaleziono kategorii";
+        }
+    }
+
     public function isEmptyCategory()
     {
         $userId = $_SESSION['user_id'];
