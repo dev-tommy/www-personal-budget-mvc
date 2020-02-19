b9c89582e4837bdc3ac301b4de9647944a583d87
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-02-19 11:16:40 +0100

    Add isEmptyMethod()

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -129,6 +129,24 @@ class Expense extends \Core\Model
         return $stmt->fetch();
     }
 
+    public function isEmptyMethod()
+    {
+        $userId = $_SESSION['user_id'];
+
+        $sql = 'SELECT * FROM expenses WHERE user_id = :userId AND payment_method_assigned_to_user_id = :methodId';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $stmt->bindValue(':methodId', $this->id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
     public function validate()
     {
         if (!isset($this->amount) || ($this->amount == '')) {
