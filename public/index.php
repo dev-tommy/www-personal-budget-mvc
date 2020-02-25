f86bf15422e51268dcb3b3549c90a235b970cf9d
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-02-25 16:55:03 +0100

    Add route get-expenses-categories-hints

--- public/index.php
+++ public/index.php
@@ -45,6 +45,7 @@ $router->add('get-all-expenses-categories', ['controller' => 'Expenses', 'action
 $router->add('get-all-payments-methods', ['controller' => 'Expenses', 'action' => 'getAllMethods']);
 $router->add('get-total-expenses-of-category', ['controller' => 'Expenses', 'action' => 'getTotalMonthlyExpenses']);
 $router->add('get-incomes-categories-hints', ['controller' => 'Incomes', 'action' => 'getCategoryNameLike']);
+$router->add('get-expenses-categories-hints', ['controller' => 'Expenses', 'action' => 'getCategoryNameLike']);
 //$router->add('{controller}/{action}');
 //$router->add('{controller}/{id:\d+}/{action}');
 //$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
