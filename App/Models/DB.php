cb8962105f74c823aaad1252d9b1fa7fa11edede
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-02-27 19:40:35 +0100

    Add findInTable()

--- App/Models/DB.php
+++ App/Models/DB.php
@@ -28,4 +28,28 @@ class DB extends \Core\Model
 
         return $stmt->execute();
     }
+
+    public static function findInTable($table, $column, $value, $datatype)
+    {
+        switch ($datatype) {
+            case "str":
+                $param = PDO::PARAM_STR;
+                break;
+            case "int":
+                $param = PDO::PARAM_INT;
+                break;
+            default:
+                $param = PDO::PARAM_NULL;
+        }
+        $sql = 'SELECT * FROM '.$table.' WHERE '.$column.' = ?';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(1, $value, $param);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
 }
